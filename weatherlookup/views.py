adec66d4e3ee83a07f5cd7f61f68145476475861
Deepak Kumar <drsdeepakkwr@gmail.com>
2023-12-31 18:53:07 +0530

    CityName Input Added

--- weatherlookup/views.py
+++ weatherlookup/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 
 def home(request):
-	import json
-	import requests
-	api_request = requests.get("http://dataservice.accuweather.com/currentconditions/v1/202396?apikey=LCCUpMujoA3AHLTxAlFA6DK0gIOBEhjB")
-	try:
-		api = json.loads(api_request.content)
-	except Exception as e:
-		api = "error"
-	return render(request,'home.html',{'api' : api})
+		import json
+		import requests
+		request.method == 'POST'
+		CityName = request.POST['cityname']
+		api_request = requests.get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+CityName+"?unitGroup=metric&key=9DJSBRXRLC97NMQC7KN83W6TF&contentType=json")
+		try:
+				api = json.loads(api_request.content)
+		except Exception as e:
+				api = "error"
+		return render(request,'home.html',{'api' : api})
 
 def about(request):
 	return render(request,'about.html',{})	
