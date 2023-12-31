a24124387fbac9004c626afdbe71e615838be880
Deepak Kumar <drsdeepakkwr@gmail.com>
2023-12-31 13:10:23 +0530

    API Data

--- weatherlookup/views.py
+++ weatherlookup/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
 def home(request):
-	return render(request,'home.html',{})
+	import json
+	import requests
+	api_request = requests.get("http://dataservice.accuweather.com/currentconditions/v1/202396?apikey=LCCUpMujoA3AHLTxAlFA6DK0gIOBEhjB")
+	try:
+		api = json.loads(api_request.content)
+	except Exception as e:
+		api = "error"
+	return render(request,'home.html',{'api' : api})
 
 def about(request):
 	return render(request,'about.html',{})	
